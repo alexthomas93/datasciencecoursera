efed9aee5f48f2920f6f864a6ef51b4a8602c92a
Alex Thomas <alex.thomas@bt.com>
2019-07-30 12:04:31 +0100

    Added R Programming week 2 assignment

---
+++ R Programming/Week 2/corr.R
@@ -0,0 +1,13 @@
+corr <- function(directory, threshold = 0) {
+  complete_cases <- complete(directory)
+  filtered = complete_cases[complete_cases["nobs"] > threshold,]
+  id = filtered[["id"]]
+  corrs <- c()
+  for (i in id) {
+    file = paste(directory, sprintf("/%03d.csv", i), sep = "")
+    df <- read.csv(file)
+    cr <- cor(df[["nitrate"]], df[["sulfate"]], use = "complete.obs")
+    corrs <- append(corrs, cr)
+  }
+  corrs
+}
---
+++ R Programming/Week 2/pollutantmean.R
@@ -0,0 +1,10 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  pollutants <- c()
+  for (i in id) {
+    file = paste(directory, sprintf("/%03d.csv", i), sep = "")
+    df <- read.csv(file)
+    new_pollutants <- na.omit(df[[pollutant]])
+    pollutants <- append(pollutants, new_pollutants)
+  }
+  mean(pollutants)
+}
