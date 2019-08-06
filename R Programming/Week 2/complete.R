982473e6b84f670c3d859bb3f152688de2f54346
Alex Thomas <alex.thomas@bt.com>
2019-08-06 15:50:54 +0100

    Fixed week 2 assignment formatting

--- R Programming/Week 2/complete.R
+++ R Programming/Week 2/complete.R
@@ -1,10 +1,10 @@
 complete <- function(directory, id = 1:332) {
-  nobs <- c()
-  for (i in id) {
-    file = paste(directory, sprintf("/%03d.csv", i), sep = "")
-    df <- read.csv(file)
-    nob <- sum(complete.cases(df))
-    nobs <- append(nobs, nob)
-  }
-  data.frame(id=id, nobs=nobs)
+    nobs <- c()
+    for (i in id) {
+        file = paste(directory, sprintf("/%03d.csv", i), sep = "")
+        df <- read.csv(file)
+        nob <- sum(complete.cases(df))
+        nobs <- append(nobs, nob)
+    }
+    data.frame(id=id, nobs=nobs)
 }
--- R Programming/Week 2/corr.R
+++ R Programming/Week 2/corr.R
@@ -1,13 +1,13 @@
 corr <- function(directory, threshold = 0) {
-  complete_cases <- complete(directory)
-  filtered = complete_cases[complete_cases["nobs"] > threshold,]
-  id = filtered[["id"]]
-  corrs <- c()
-  for (i in id) {
-    file = paste(directory, sprintf("/%03d.csv", i), sep = "")
-    df <- read.csv(file)
-    cr <- cor(df[["nitrate"]], df[["sulfate"]], use = "complete.obs")
-    corrs <- append(corrs, cr)
-  }
-  corrs
+    complete_cases <- complete(directory)
+    filtered = complete_cases[complete_cases["nobs"] > threshold,]
+    id = filtered[["id"]]
+    corrs <- c()
+    for (i in id) {
+        file = paste(directory, sprintf("/%03d.csv", i), sep = "")
+        df <- read.csv(file)
+        cr <- cor(df[["nitrate"]], df[["sulfate"]], use = "complete.obs")
+        corrs <- append(corrs, cr)
+    }
+    corrs
 }
